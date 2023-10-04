85d48bfeb1e500bf6d734ac7e8125db868fb40ef
Daniel Her <danieljhher@gmail.com>
2023-10-04 15:01:06 -0700

    Initial Commit

---
+++ C867/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C867
+//
+//  Created by Daniel Her on 10/4/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
