d80a7244aed713273d7bb8e8912ade4fed505da5
Daniel Her <danieljhher@gmail.com>
2023-10-04 15:07:28 -0700

    added README

--- C867/main.cpp
+++ C867/main.cpp
@@ -1,9 +1,3 @@
-//
-//  main.cpp
-//  C867
-//
-//  Created by Daniel Her on 10/4/23.
-//
 
 #include <iostream>
 
@@ -12,4 +6,4 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     return 0;
 }
- //yo
+
